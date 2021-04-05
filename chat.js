0f4c9fd125be70f4b63869d9524d2829a0a2a399
JiahuiChen99 <jiahui.xie@students.salle.url.edu>
2021-04-05 12:45:30 +0200

    #2 feat: Chat UI improved

--- chat.js
+++ chat.js
@@ -4,6 +4,10 @@ const service = require('./proto/chatapi_grpc_pb')
 
 
 const client = new service.ChatbotClient('localhost:9090', grpc.credentials.createInsecure(), null);
+
+const user_logo = './assets/ahri_icon.png';
+const bot_logo = './assets/lol.png';
+
 function sendMessage() {
     let user_input = document.getElementById('user-input').value;
 
@@ -11,12 +15,43 @@ function sendMessage() {
         let message_section = document.getElementById('messages');
 
         // Create new User message with styles
+        let chat_container = document.createElement("div");
+        chat_container.classList.add("chat-container", "block", "user");
+        let chat_bubble = document.createElement("div");
+        chat_bubble.classList.add("chat-bubble");
+        let info = document.createElement("div");
+        info.classList.add("info");
+
+        let time = document.createElement("span");
+        let time_data = new Date();
+        time.innerText = time_data.getHours() + ':' + time_data.getMinutes();
+
+        let username = document.createElement("span");
+        username.classList.add("username");
+        username.innerText = "@Nako";
+
+        info.appendChild(time);
+        info.appendChild(username);
+
         let new_message = document.createElement("div");
         new_message.innerText = user_input;
         new_message.classList.add("bubble-user", "block");
 
+        chat_bubble.appendChild(info);
+        chat_bubble.appendChild(new_message);
+
+        let figure = document.createElement("figure");
+        figure.classList.add("image", "is-48x48", "level-right", "user-logo");
+        let img = document.createElement("img");
+        img.src = user_logo;
+        img.classList.add("is-rounded");
+        figure.appendChild(img);
+
+        chat_container.appendChild(chat_bubble);
+        chat_container.appendChild(figure);
+
         // Insert new User message
-        message_section.appendChild(new_message);
+        message_section.appendChild(chat_container);
 
         //Scroll to bottom
         message_section.scrollTop = message_section.scrollHeight;
